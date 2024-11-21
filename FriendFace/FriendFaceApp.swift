abb9060d0eebbc78b835decda46a72ce33cc15b2
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-11-21 05:20:32 +0100

    Initial Commit

---
+++ FriendFace/FriendFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendFaceApp.swift
+//  FriendFace
+//
+//  Created by Jakub Czerwiec  on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FriendFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
